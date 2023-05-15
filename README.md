d582370ee02ea6db96c19d934f8edbc266459cb0
Nathan Hettige <nathan.hettige@gmail.com>
2023-05-15 11:45:08 +1000

    take response status as path param

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # httprespond
-Go webhook that throws http errors to test http exception handling against
+Go server that returns http errors to test http exception handling against.
+
+Request /400 for a 400 response, /300 for a 300 response etc.
--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
-	"github.com/nathanhettige/httprespond/internal/responses"
 )
 
 func main() {
 	router := gin.Default()
-
-	router.Any("/200", responses.TwoHundred)
-	router.Any("/300", responses.ThreeHundred)
-	router.Any("/400", responses.FourHundred)
-
+	router.Any("/:input", httpResponse)
 	router.Run(":3000")
+}
+
+func httpResponse(c *gin.Context) {
+	input := c.Param("input")
+
+	status, err := strconv.Atoi(input)
+	if err != nil {
+		c.String(http.StatusNotAcceptable, fmt.Sprintf("Invalid http response status code - %s.", input))
+		return
+	}
+
+	if http.StatusText(status) == "" {
+		c.String(http.StatusNotAcceptable, fmt.Sprintf("HTTP Status code %d doesn't exist.", status))
+		return
+	}
+
+	c.String(status, fmt.Sprintf("%d %s", status, http.StatusText(status)))
 }
